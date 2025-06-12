c59325ff691bbb6faa3c3983235c3e67acebe9aa
Prashik Kamble <138839248+prashik536@users.noreply.github.com>
2025-06-13 02:00:16 +0530

    Update GoogleSearchTest.java

--- src/test/java/com/example/GoogleSearchTest.java
+++ src/test/java/com/example/GoogleSearchTest.java
@@ -7,25 +7,39 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
 
 public class GoogleSearchTest {
 
-    WebDriver driver;
+    private WebDriver driver;
 
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        driver = new ChromeDriver();
+        // Add Chrome options
+        ChromeOptions options = new ChromeOptions();
+
+        // Avoid session error by setting a unique user data dir
+        options.addArguments("--user-data-dir=/tmp/selenium-profile-" + System.currentTimeMillis());
+
+        // Headless for Jenkins/Docker
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
     }
 
     @Test
-    public void testGoogleSearch() throws InterruptedException {
+    public void testGoogleSearch() {
         driver.get("https://www.google.com");
-        WebElement input = driver.findElement(By.name("q"));
-        input.sendKeys("Jenkins Selenium test");
-        input.submit();
-        Thread.sleep(2000);
-        System.out.println("Title: " + driver.getTitle());
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("OpenAI");
+        searchBox.submit();
+        assertTrue(driver.getTitle().contains("OpenAI"));
     }
 
     @After
